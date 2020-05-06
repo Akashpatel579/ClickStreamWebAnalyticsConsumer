83a45149954748dec5751ac4a47ab042043dcfc8
Akash Patel <akashpatel0509@gmail.com>
2020-05-05 22:58:17 -0400

    Hello World

---
+++ ClickStreamConsumer.py
@@ -0,0 +1,36 @@
+print("Hello World!")
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import get_json_object
+
+spark = SparkSession. \
+    builder. \
+    appName("WebAalyticsApp"). \
+    config("spark.ui.port", "0"). \
+    getOrCreate()
+
+
+clicks_df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "click_events") \
+    .option("startingOffsets", "latest") \
+    .option("inferSchema", "true") \
+    .load() \
+    .selectExpr("CAST(value AS STRING)").alias("json_data")  # can remove alias
+
+# query = clicks_df. \
+#     writeStream. \
+#     trigger(processingTime='10 seconds'). \
+#     format("console"). \
+#     start()
+
+query = clicks_df. \
+    writeStream. \
+    queryName("clickevents"). \
+    format("memory"). \
+    outputMode("append"). \
+    start()
+
+query.awaitTermination()
